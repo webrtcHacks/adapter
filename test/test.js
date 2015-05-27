16add0df44b1b2fc181c796aa056f26f9b63b7e6
Philipp Hancke <fippo@andyet.net>
2015-05-26 18:57:12 -0700

    follow signal-with-setLocalDescription pattern

--- test/test.js
+++ test/test.js
@@ -54,48 +54,48 @@ test('basic connection establishment', function(t) {
       pc1.setLocalDescription(offer,
         function() {
           t.pass('pc1.setLocalDescription');
-        },
-        function(err) {
-          t.fail('pc1.setLocalDescription ' + err.toString());
-        }
-      );
 
-      offer = JSON.parse(JSON.stringify(offer));
-      offer = new RTCSessionDescription(offer);
-      t.pass('created RTCSessionDescription from offer');
-      pc2.setRemoteDescription(offer,
-        function() {
-          t.pass('pc2.setRemoteDescription');
-          pc2.createAnswer(
-            function(answer) {
-              t.pass('pc2.createAnswer');
-              pc2.setLocalDescription(answer,
-                function() {
-                  t.pass('pc2.setLocalDescription');
+          offer = JSON.parse(JSON.stringify(offer));
+          offer = new RTCSessionDescription(offer);
+          t.pass('created RTCSessionDescription from offer');
+          pc2.setRemoteDescription(offer,
+            function() {
+              t.pass('pc2.setRemoteDescription');
+              pc2.createAnswer(
+                function(answer) {
+                  t.pass('pc2.createAnswer');
+                  pc2.setLocalDescription(answer,
+                    function() {
+                      t.pass('pc2.setLocalDescription');
+                      answer = JSON.parse(JSON.stringify(answer));
+                      answer = new RTCSessionDescription(answer);
+                      t.pass('created RTCSessionDescription from answer');
+                      pc1.setRemoteDescription(answer,
+                        function() {
+                          t.pass('pc1.setRemoteDescription');
+                        },
+                        function(err) {
+                          t.fail('pc1.setRemoteDescription ' + err.toString());
+                        }
+                      );
+                    },
+                    function(err) {
+                      t.fail('pc2.setLocalDescription ' + err.toString());
+                    }
+                  );
                 },
                 function(err) {
-                  t.fail('pc2.setLocalDescription ' + err.toString());
-                }
-              );
-              answer = JSON.parse(JSON.stringify(answer));
-              answer = new RTCSessionDescription(answer);
-              t.pass('created RTCSessionDescription from answer');
-              pc1.setRemoteDescription(answer,
-                function() {
-                  t.pass('pc1.setRemoteDescription');
-                },
-                function(err) {
-                  t.fail('pc1.setRemoteDescription ' + err.toString());
+                  t.fail('pc2.createAnswer ' + err.toString());
                 }
               );
             },
             function(err) {
-              t.fail('pc2.createAnswer ' + err.toString());
+              t.fail('pc2.setRemoteDescription ' + err.toString());
             }
-      );
+          );
         },
         function(err) {
-          t.fail('pc2.setRemoteDescription ' + err.toString());
+          t.fail('pc1.setLocalDescription ' + err.toString());
         }
       );
     },
