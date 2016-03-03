970fd34c66e91354cdbf7cf8a5b1e124a9bbfd72
Michele Olivo <mo@zextras.com>
2016-03-03 10:12:34 +0100

    Added missing semicolon on utils.js
    Some ancient browsers (like IE 8) Cannot handle correctly the fact that a semicolon is missing at the end of the row.
    With this modification the code will run smooth on IE 8.

--- src/js/utils.js
+++ src/js/utils.js
@@ -51,7 +51,7 @@ var utils = {
     // Returned result object.
     var result = {};
     result.browser = null;
-    result.version = null
+    result.version = null;
     result.minVersion = null;
 
     // Non supported browser.
