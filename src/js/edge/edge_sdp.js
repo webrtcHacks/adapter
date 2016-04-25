54496d4d27fd86ff3a343cd0144b777fcf7a0941
Philipp Hancke <philipp.hancke@googlemail.com>
2016-04-25 19:06:22 +0200

    parse rtx pt as well

--- src/js/edge/edge_sdp.js
+++ src/js/edge/edge_sdp.js
@@ -390,6 +390,7 @@ SDPUtils.parseRtpEncodingParameters = function(mediaSection) {
         ssrc: primarySsrc,
         codecPayloadType: parseInt(codec.parameters.apt, 10),
         rtx: {
+          payloadType: codec.payloadType,
           ssrc: secondarySsrc
         }
       };
