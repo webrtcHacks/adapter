8f56184c83b704a56d8edf0cc1f97cb78761a22f
Jan-Ivar Bruaroey <wingboy@gmail.com>
2016-07-23 22:28:37 -0400

    gUM's provided media MUST include one track of each requested type.

--- src/js/firefox/getusermedia.js
+++ src/js/firefox/getusermedia.js
@@ -133,7 +133,15 @@ module.exports = function() {
     var origGetUserMedia = navigator.mediaDevices.getUserMedia.
         bind(navigator.mediaDevices);
     navigator.mediaDevices.getUserMedia = function(c) {
-      return origGetUserMedia(c).catch(function(e) {
+      return origGetUserMedia(c).then(function(stream) {
+        // Work around https://bugzil.la/802326
+        if (c.audio && !stream.getAudioTracks().length ||
+            c.video && !stream.getVideoTracks().length) {
+          throw new DOMException('The object can not be found here.',
+                                 'NotFoundError');
+        }
+        return stream;
+      }, function(e) {
         return Promise.reject(shimError_(e));
       });
     };
