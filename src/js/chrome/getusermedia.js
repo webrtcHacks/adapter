6242eb02a74560454d3f9bd73d6d361858b4d19c
Jan-Ivar Bruaroey <wingboy@gmail.com>
2016-07-25 14:59:23 -0400

    Not needed in Chrome in the end.

--- src/js/chrome/getusermedia.js
+++ src/js/chrome/getusermedia.js
@@ -167,13 +167,7 @@ module.exports = function() {
         bind(navigator.mediaDevices);
     navigator.mediaDevices.getUserMedia = function(cs) {
       return shimConstraints_(cs, function(c) {
-        return origGetUserMedia(c).then(function(stream) {
-          if (c.audio && !stream.getAudioTracks().length ||
-              c.video && !stream.getVideoTracks().length) {
-            throw new DOMException('', 'NotFoundError');
-          }
-          return stream;
-        }, function(e) {
+        return origGetUserMedia(c).catch(function(e) {
           return Promise.reject(shimError_(e));
         });
       });
