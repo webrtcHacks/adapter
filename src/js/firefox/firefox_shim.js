d8453c70bb3fd65357c02cc882a02cc9b91c58d8
Philipp Hancke <fippo@appear.in>
2016-10-24 14:43:14 +0200

    addIceCandidate(null): call callbacks
    currently this only resolves the promise.

--- src/js/firefox/firefox_shim.js
+++ src/js/firefox/firefox_shim.js
@@ -121,8 +121,13 @@ var firefoxShim = {
     var nativeAddIceCandidate =
         RTCPeerConnection.prototype.addIceCandidate;
     RTCPeerConnection.prototype.addIceCandidate = function() {
-      return arguments[0] === null ? Promise.resolve()
-          : nativeAddIceCandidate.apply(this, arguments);
+      if (arguments[0] === null) {
+        if (arguments[1]) {
+          arguments[1].apply(null);
+        }
+        return Promise.resolve();
+      }
+      return nativeAddIceCandidate.apply(this, arguments);
     };
 
     // shim getStats with maplike support
