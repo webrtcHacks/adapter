c6704aa0d4c226b5fb154f18c8965698319cf32e
Philipp Hancke <fippo@appear.in>
2017-03-13 19:30:02 +0100

    test: move log supression test
    makes the log suppression test a unit test.

---
+++ test/unit/logSuppression.js
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+/* eslint-env node */
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Log suppression', () => {
+  const utils = require('../../src/js/utils.js');
+  const saveConsole = console.log.bind(console);
+
+  let logCount;
+  beforeEach(() => {
+    logCount = 0;
+    console.log = function() {
+      if (arguments.length === 1 && arguments[0] === 'test') {
+        logCount++;
+      } else {
+        saveConsole.apply(saveConsole, arguments);
+      }
+    };
+    global.window = {};
+    require('../../out/adapter.js');
+  });
+
+  afterEach(() => {
+    console.log = saveConsole;
+    delete global.window;
+  });
+
+  it('does not call console.log by default', () => {
+    utils.log('test');
+    expect(logCount).to.equal(0);
+  });
+  it('does call console.log when enabled', () => {
+    utils.disableLog(false);
+    utils.log('test');
+    expect(logCount).to.equal(1);
+  });
+});
