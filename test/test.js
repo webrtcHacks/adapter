3649f8bb6c09303cd6cf480087277fa3f95ee62e
Philipp Hancke <fippo@appear.in>
2017-04-07 10:15:51 +0200

    test: add videoElement.loadedmetadata test
    adds a test which is similar to the web-platform-test simplecall test
      https://github.com/w3c/web-platform-tests/blob/master/webrtc/simplecall.html
    
    Would have probably caught #487.

--- test/test.js
+++ test/test.js
@@ -1457,6 +1457,128 @@ test('Basic connection establishment with datachannel', function(t) {
   });
 });
 
+test('video loadedmetadata is called for a video call', function(t) {
+  var driver = seleniumHelpers.buildDriver();
+
+  var testDefinition = function() {
+    var callback = arguments[arguments.length - 1];
+
+    var counter = 1;
+    window.testPassed = [];
+    window.testFailed = [];
+    var tc = {
+      ok: function(ok, msg) {
+        window[ok ? 'testPassed' : 'testFailed'].push(msg);
+      },
+      is: function(a, b, msg) {
+        this.ok((a === b), msg + ' - got ' + b);
+      },
+      pass: function(msg) {
+        this.ok(true, msg);
+      },
+      fail: function(msg) {
+        this.ok(false, msg);
+      }
+    };
+    var pc1 = new RTCPeerConnection(null);
+    var pc2 = new RTCPeerConnection(null);
+
+    pc2.addEventListener('addstream', function(e) {
+      var v = document.createElement('video');
+      v.autoplay = true;
+      v.addEventListener('loadedmetadata', function() {
+        tc.pass('loadedmetadata');
+        callback();
+      });
+      v.srcObject = e.stream;
+    });
+    var dictionary = obj => JSON.parse(JSON.stringify(obj));
+
+    var addCandidate = function(pc, event) {
+      if (event.candidate) {
+        event.candidate = dictionary(event.candidate);
+      }
+      pc.addIceCandidate(event.candidate).then(function() {
+        // TODO: Decide if we are interested in adding all candidates
+        // as passed tests.
+        tc.pass('addIceCandidate ' + counter++);
+      })
+      .catch(function(err) {
+        tc.fail('addIceCandidate ' + err.toString());
+      });
+    };
+    pc1.onicecandidate = function(event) {
+      addCandidate(pc2, event);
+    };
+    pc2.onicecandidate = function(event) {
+      addCandidate(pc1, event);
+    };
+
+    var constraints = {audio: true, video: true, fake: true};
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+      pc1.addStream(stream);
+      pc1.createOffer().then(function(offer) {
+        tc.pass('pc1.createOffer');
+        return pc1.setLocalDescription(dictionary(offer));
+      }).then(function() {
+        tc.pass('pc1.setLocalDescription');
+        return pc2.setRemoteDescription(dictionary(pc1.localDescription));
+      }).then(function() {
+        tc.pass('pc2.setRemoteDescription');
+        return pc2.createAnswer();
+      }).then(function(answer) {
+        tc.pass('pc2.createAnswer');
+        return pc2.setLocalDescription(dictionary(answer));
+      }).then(function() {
+        tc.pass('pc2.setLocalDescription');
+        return pc1.setRemoteDescription(dictionary(pc2.localDescription));
+      }).then(function() {
+        tc.pass('pc1.setRemoteDescription');
+      }).catch(function(err) {
+        tc.fail(err.toString());
+      });
+    })
+    .catch(function(error) {
+      callback(error);
+    });
+  };
+
+  // Run test.
+  seleniumHelpers.loadTestPage(driver)
+  .then(function() {
+    t.pass('Page loaded');
+    return driver.executeAsyncScript(testDefinition);
+  })
+  .then(function(error) {
+    // Callback will either return an error object or pc1ConnectionStatus.
+    if (error) {
+      throw(error);
+    }
+    return driver.executeScript('return window.testPassed');
+  })
+  .then(function(testPassed) {
+    return driver.executeScript('return window.testFailed')
+    .then(function(testFailed) {
+      for (var testPass = 0; testPass < testPassed.length; testPass++) {
+        t.pass(testPassed[testPass]);
+      }
+      for (var testFail = 0; testFail < testFailed.length; testFail++) {
+        t.fail(testFailed[testFail]);
+      }
+    });
+  })
+  .then(function() {
+    t.end();
+  })
+  .then(null, function(err) {
+    if (err !== 'skip-test') {
+      t.fail(err);
+    }
+    t.end();
+  });
+});
+
 test('dtmf', t => {
   var driver = seleniumHelpers.buildDriver();
 
