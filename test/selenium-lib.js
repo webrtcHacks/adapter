388eafcc8fce6b6f90b40b8f84ae089aac2e328e
Philipp Hancke <fippo@appear.in>
2017-06-19 09:53:06 +0200

    test: use travis for safari technology preview
    WIP, testing how easy @aullman made it.

--- test/selenium-lib.js
+++ test/selenium-lib.js
@@ -93,7 +93,7 @@ function buildDriver() {
   var edgeOptions = new edge.Options();
 
   var safariOptions = new safari.Options();
-  safariOptions.setTechnologyPreview(process.env.BVER === 'TechnologyPreview');
+  safariOptions.setTechnologyPreview(process.env.BVER === 'unstable');
 
   sharedDriver = new webdriver.Builder()
       .forBrowser(process.env.BROWSER)
