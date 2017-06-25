bcbdd55b8174fa40c779b3bdcffb10e2f6293bb0
Philipp Hancke <fippo@appear.in>
2017-06-25 16:07:11 +0200

    test: port MediaStream shim test

--- test/test.js
+++ test/test.js
@@ -55,26 +55,6 @@ test('navigator.mediaDevices eventlisteners', function(t) {
   });
 });
 
-test('MediaStream shim', function(t) {
-  var driver = seleniumHelpers.buildDriver();
-
-  // Run test.
-  seleniumHelpers.loadTestPage(driver)
-  .then(function() {
-    t.pass('Page loaded');
-    return driver.executeScript(
-      'return window.MediaStream !== \'undefined\'');
-  })
-  .then(function(isMediaStreamDefined) {
-    t.ok(isMediaStreamDefined, 'MediaStream is defined');
-    t.end();
-  })
-  .then(null, function(err) {
-    t.fail(err);
-    t.end();
-  });
-});
-
 test('RTCPeerConnection shim', function(t) {
   var driver = seleniumHelpers.buildDriver();
 
