b5137eb2402fb24047d2e232d8d35279606d0dac
Philipp Hancke <fippo@appear.in>
2017-12-27 13:11:26 +0100

    test: add test for fiddling with localDescription
    as promised fixes #651

--- test/e2e/msid.js
+++ test/e2e/msid.js
@@ -11,6 +11,7 @@
 describe('MSID', () => {
   let pc1;
   let pc2;
+  let localStream;
 
   function negotiate(pc, otherPc) {
     return pc.createOffer()
@@ -40,7 +41,6 @@ describe('MSID', () => {
   });
 
   it('signals track and stream ids', (done) => {
-    let localStream;
     pc2.ontrack = (e) => {
       expect(e.track.id).to.equal(localStream.getTracks()[0].id);
       expect(e.streams[0].id).to.equal(localStream.id);
@@ -51,6 +51,20 @@ describe('MSID', () => {
       localStream = stream;
       pc1.addTrack(stream.getTracks()[0], stream);
       return negotiate(pc1, pc2);
+    });
+  });
+
+  it('puts the track msid attribute into the localDescription', () => {
+    return navigator.mediaDevices.getUserMedia({video: true})
+    .then((stream) => {
+      localStream = stream;
+      pc1.addTrack(stream.getTracks()[0], stream);
+      return negotiate(pc1, pc2);
+    })
+    .then(() => {
+      const track = localStream.getTracks()[0];
+      expect(pc1.localDescription.sdp)
+          .to.contain('msid:' + localStream.id + ' ' + track.id);
     });
   });
 });
