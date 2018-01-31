ceba275ba9e67e6ef6d0b71fe53c71514fc9fb7c
Francesco Durighetto <work@francescodurighetto.com>
2018-01-31 10:22:22 +0100

    Added correct direction for new firefox 59 transceiver implementation

--- src/js/adapter_factory.js
+++ src/js/adapter_factory.js
@@ -94,6 +94,7 @@ module.exports = function(dependencies, opts) {
       firefoxShim.shimPeerConnection(window);
       firefoxShim.shimOnTrack(window);
       firefoxShim.shimRemoveStream(window);
+      firefoxShim.shimCreateOfferLegacy(window);
 
       commonShim.shimRTCIceCandidate(window);
       commonShim.shimMaxMessageSize(window);
