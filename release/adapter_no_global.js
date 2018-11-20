aa903f03136786810ca1b8cc661a7e0cbd6dc3f0
Philipp Hancke <fippo@appear.in>
2018-11-20 18:53:20 +0100

    Add adapter artifacts

--- release/adapter_no_global.js
+++ release/adapter_no_global.js
@@ -4315,12 +4315,12 @@ module.exports = {
     }
     var origGetDisplayMedia = window.navigator.getDisplayMedia;
     window.navigator.mediaDevices.getDisplayMedia = function(constraints) {
-      return origGetDisplayMedia(constraints);
+      return origGetDisplayMedia.call(window.navigator, constraints);
     };
     window.navigator.getDisplayMedia = function(constraints) {
       utils.deprecated('navigator.getDisplayMedia',
           'navigator.mediaDevices.getDisplayMedia');
-      return origGetDisplayMedia(constraints);
+      return origGetDisplayMedia.call(window.navigator, constraints);
     };
   }
 };
