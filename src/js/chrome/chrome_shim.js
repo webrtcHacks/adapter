001de0e682f3855258731e8c6ca205153ebc5d24
leochliu <leochliu@tencent.com>
2026-05-25 20:00:48 +0800

    fix: add null check for localDescription to prevent crash on SLBrowser

--- src/js/chrome/chrome_shim.js
+++ src/js/chrome/chrome_shim.js
@@ -550,7 +550,7 @@ export function shimAddTrackRemoveTrack(window, browserDetails) {
     'localDescription', {
       get() {
         const description = origLocalDescription.get.apply(this);
-        if (description.type === '') {
+        if (!description || description.type === '') {
           return description;
         }
         return replaceInternalStreamId(this, description);
